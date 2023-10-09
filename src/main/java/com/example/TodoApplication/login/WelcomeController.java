351fb6a50f51a35a18132d12444831cfb6a4bb49
Prathamesh Zingade <zingadeprathamesh12@gmail.com>
2023-10-09 20:56:46 +0530

    added the spring security

--- src/main/java/com/example/TodoApplication/login/WelcomeController.java
+++ src/main/java/com/example/TodoApplication/login/WelcomeController.java
@@ -1,17 +1,19 @@
 package com.example.TodoApplication.login;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
 @SessionAttributes("name")
-public class LoginController {
+public class WelcomeController {
 
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String loginPage() {
+	public String welcomePage(ModelMap model) {
+		model.put("name", "Prathamesh");
 		return "welcome";
 	}
 	
---
+++ src/main/java/com/example/TodoApplication/security/SpringSecurityConfig.java
@@ -0,0 +1,36 @@
+package com.example.TodoApplication.security;
+
+import java.util.function.Function;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class SpringSecurityConfig {
+	
+	@Bean
+	public InMemoryUserDetailsManager createUserDetailsManager() {
+		Function<String, String> passWordEncoder =
+				input -> passwordEncoder().encode(input);
+				
+				
+		UserDetails userDetails = User.builder()
+				.passwordEncoder(passWordEncoder )
+				.username("prathamesh")
+				.password("dummy")
+				.roles("USER", "ADMIN")
+				.build();		
+		return new InMemoryUserDetailsManager(userDetails);
+	}
+	
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+}
