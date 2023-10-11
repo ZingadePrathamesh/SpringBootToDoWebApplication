7cd768df09c3ea28313462bc72a241707a26c9d5
Prathamesh Zingade <zingadeprathamesh12@gmail.com>
2023-10-11 19:20:39 +0530

    enhancing security

--- src/main/java/com/example/TodoApplication/security/SpringSecurityConfig.java
+++ src/main/java/com/example/TodoApplication/security/SpringSecurityConfig.java
@@ -1,14 +1,17 @@
 package com.example.TodoApplication.security;
+import static org.springframework.security.config.Customizer.withDefaults;
 
 import java.util.function.Function;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SpringSecurityConfig {
@@ -42,4 +45,13 @@ public class SpringSecurityConfig {
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
+	
+	private SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests(
+				auth -> auth.anyRequest().authenticated());
+		http.formLogin(withDefaults());
+		http.csrf().disable();
+		http.headers().frameOptions().disable();
+		
+		return http.build();	}
 }
